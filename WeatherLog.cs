b910acdb611649367d370609bdc91abb2a9a8793
Luan1006 <thenoobsisbest@gmail.com>
2024-04-18 12:31:32 +0200

    Created WeatherLog Class

---
+++ WeatherLog.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Luan1006.MM202.ExamUnit4
+{
+    public class WeatherLog
+    {
+        public List<WeatherData> Data { get; set; } = new List<WeatherData>();
+
+        public void AddData(WeatherData data)
+        {
+            Data.Add(data);
+        }
+
+        public WeatherData GetData(DateTime date)
+        {
+            return Data.FirstOrDefault(d => d.Date.Date == date.Date);
+        }
+    }
+}
