90fff68f5ca3b8ac8c7782629baadf57a818f5fe
Luan1006 <thenoobsisbest@gmail.com>
2024-04-18 12:28:48 +0200

    updated WeatherData class

---
+++ WeatherData.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Luan1006.MM202.ExamUnit4
+{
+    public class WeatherData
+    {
+        public DateTime Date { get; set; }
+        public double Temperature { get; set; }
+
+        // Constructor
+        public WeatherData(DateTime date, double temperature)
+        {
+            Date = date;
+            Temperature = temperature;
+        }
+    }
+}
