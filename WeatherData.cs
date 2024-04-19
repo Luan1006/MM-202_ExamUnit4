8cb2eb585779fc8e03933a090e2ce5036bda3935
Luan1006 <thenoobsisbest@gmail.com>
2024-04-19 17:32:19 +0200

    Removed unnecesary comment

--- WeatherData.cs
+++ WeatherData.cs
@@ -5,7 +5,6 @@ namespace Luan1006.MM202.ExamUnit4
         public DateTime Date { get; set; }
         public double Temperature { get; set; }
 
-        // Constructor
         public WeatherData(DateTime date, double temperature)
         {
             Date = date;
