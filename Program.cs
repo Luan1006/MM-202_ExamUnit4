46c5939b55bf163749e001d947338f0b5b88645d
Luan1006 <thenoobsisbest@gmail.com>
2024-04-27 17:46:05 +0200

    Added some user UI

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         public static void Main()
         {
-            
+            Console.WriteLine("Welcome to the weatherlog!");
+            Console.WriteLine("Please enter your location: ");
+            string location = Console.ReadLine();
+            Console.WriteLine("Please enter the temperature in celsius: ");
+            double temperature = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the date (yyyy-MM-dd) or : ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
         }
     }
 }
