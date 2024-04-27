db8346004228d34807ea9dc7292fa8f3236d9d30
Luan1006 <thenoobsisbest@gmail.com>
2024-04-27 18:32:21 +0200

    removed user ui

--- Program.cs
+++ Program.cs
@@ -6,14 +6,6 @@ namespace Luan1006.MM202.ExamUnit4
     {
         public static void Main()
         {
-            // Console.WriteLine("Welcome to the weatherlog!");
-            // Console.WriteLine("Please enter your location: ");
-            // string location = Console.ReadLine();
-            // Console.WriteLine("Please enter the temperature in celsius: ");
-            // double temperature = double.Parse(Console.ReadLine());
-            // Console.WriteLine("Please enter the date (yyyy-MM-dd) or : ");
-            // DateTime date = DateTime.Parse(Console.ReadLine());
-
             double latitude = 58.20; // grimstad latitude
             double longitude = 8.35; // grimstad longitude
 
