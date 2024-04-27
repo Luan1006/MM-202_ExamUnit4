a6dea84ce86fdb858ece38247e2a229a0c6055b5
Luan1006 <thenoobsisbest@gmail.com>
2024-04-27 19:03:29 +0200

    Removed precpitation amount from WeatherData class and

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace Luan1006.MM202.ExamUnit4
             MetApiHandler metApiHandler = new MetApiHandler();
             JsonDocument data = metApiHandler.HandleRequest(latitude, longitude).Result;
             Console.WriteLine(data.RootElement.GetProperty("properties").GetProperty("timeseries")[0].GetProperty("data").GetProperty("instant").GetProperty("details").GetProperty("air_temperature").GetDouble());
+            Console.WriteLine(data.RootElement.GetProperty("properties").GetProperty("timeseries")[0].GetProperty("data").GetProperty("instant").GetProperty("details").GetProperty("relative_humidity").GetDouble());
+            Console.WriteLine(data.RootElement.GetProperty("properties").GetProperty("timeseries")[0].GetProperty("data").GetProperty("instant").GetProperty("details").GetProperty("wind_from_direction").GetDouble());
+            Console.WriteLine(data.RootElement.GetProperty("properties").GetProperty("timeseries")[0].GetProperty("data").GetProperty("instant").GetProperty("details").GetProperty("wind_speed").GetDouble());
         }
     }
 }
--- WeatherData.cs
+++ WeatherData.cs
@@ -2,24 +2,23 @@ namespace Luan1006.MM202.ExamUnit4
 {
     public class WeatherData
     {
-        private DateTime Date { get; set; }
-        private double Longitude { get; set; }
-        private double Latitude { get; set; }
-        private double AirTemperature { get; set; }
-        private double PrecipitationAmount { get; set; }
-        private double RelativeHumidity { get; set; }
-        private double WindFromDirection { get; set; }
-        private double WindSpeed { get; set; }
+        public DateTime Date { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double AirTemperature { get; set; }
+        public double RelativeHumidity { get; set; }
+        public double WindFromDirection { get; set; }
+        public double WindSpeed { get; set; }
 
-        public WeatherData(DateTime date, double longitude, double latitude, double air_temperature, double precipitation_amount, double relative_humidity, double wind_from_direction, double wind_speed)
+        public WeatherData(DateTime date, double longitude, double latitude, double air_temperature, double relative_humidity, double wind_from_direction, double wind_speed)
         {
             Longitude = longitude;
             Latitude = latitude;
             Date = date;
             AirTemperature = air_temperature;
-            PrecipitationAmount = precipitation_amount;
             RelativeHumidity = relative_humidity;
             WindFromDirection = wind_from_direction;
             WindSpeed = wind_speed;
         }
     }
+}
