68d6b65377b87b22ed2a6f0e4c4733a15512fbc7
Luan1006 <thenoobsisbest@gmail.com>
2024-04-27 18:31:57 +0200

    updated weather data class

--- WeatherData.cs
+++ WeatherData.cs
@@ -3,12 +3,19 @@ namespace Luan1006.MM202.ExamUnit4
     public class WeatherData
     {
         public DateTime Date { get; set; }
-        public double Temperature { get; set; }
+        public double AirTemperature { get; set; }
+        public double PrecipitationAmount { get; set; }
+        public double RelativeHumidity { get; set; }
+        public double WindFromDirection { get; set; }
+        public double WindSpeed { get; set; }
 
-        public WeatherData(DateTime date, double temperature)
+        public WeatherData(DateTime date, double air_temperature, double precipitation_amount, double relative_humidity, double wind_from_direction, double wind_speed)
         {
             Date = date;
-            Temperature = temperature;
+            AirTemperature = air_temperature;
+            PrecipitationAmount = precipitation_amount;
+            RelativeHumidity = relative_humidity;
+            WindFromDirection = wind_from_direction;
+            WindSpeed = wind_speed;
         }
-    }
 }
