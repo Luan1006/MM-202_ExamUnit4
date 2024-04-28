0312999a53f33919a955786fb0005e78707bf6bc
Luan1006 <thenoobsisbest@gmail.com>
2024-04-28 22:23:30 +0200

    Removed unnecessary wip method

--- Program.cs
+++ Program.cs
@@ -7,23 +7,5 @@
             MainMenu userInterface = new MainMenu();
             userInterface.NavigateMenu();
         }
-
-        private static void WIP() // Work in progress - not used in the program
-        {
-            double latitude = 58.20; // grimstad latitude
-            double longitude = 8.35; // grimstad longitude
-
-            WeatherData userWeatherData = UserInput.GetWeatherData(latitude, longitude);
-            WeatherLog userWeatherLog = new WeatherLog(isUser: true);
-            userWeatherLog.AddData(userWeatherData);
-            userWeatherLog.SaveToJson("JsonFiles/WeatherLogFromUser.json");
-
-            MetApiHandler metApiHandler = new MetApiHandler(latitude, longitude);
-
-            WeatherData weatherData = metApiHandler.GetWeatherData();
-            WeatherLog weatherLog = new WeatherLog(isUser: false);
-            weatherLog.AddData(weatherData);
-            weatherLog.SaveToJson("JsonFiles/WeatherLogFromAPI.json");
-        }
     }
 }
